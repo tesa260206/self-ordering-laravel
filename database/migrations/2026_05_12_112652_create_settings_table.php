d6711ff870e28de99f85119507aa324ca5078dae
MUHAMMAD RANDY KARNA <muhammadrandykarna@gmail.com>
2026-05-24 18:55:07 +0700

    update table settings

--- database/migrations/2026_05_12_112652_create_settings_table.php
+++ database/migrations/2026_05_12_112652_create_settings_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
+            $table->string('resto_name')->default('Self Order System');
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->decimal('tax', 5, 2)->default(0); // Persentase Pajak, misal 11.00
+            $table->decimal('service_charge', 5, 2)->default(0); // Persentase Service Charge
+            $table->string('logo')->nullable();
             $table->timestamps();
         });
     }
