90e1b20cb6fac8050ec1e31727274339c287c216
MUHAMMAD RANDY KARNA <muhammadrandykarna@gmail.com>
2026-05-24 19:08:48 +0700

    update eror orer checkout

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,5 +24,16 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleAndUserSeeder::class,
         ]);
+
+        // Berikan nilai default untuk Setting agar aplikasi tidak error saat migrate:fresh
+        if (\App\Models\Setting::count() === 0) {
+            \App\Models\Setting::create([
+                'resto_name'            => 'Self Order System',
+                'tax'                   => 11,
+                'service_charge'        => 5,
+                'thermal_printer_width' => '80mm',
+                'receipt_footer_text'   => 'Terima Kasih atas kunjungan Anda!',
+            ]);
+        }
     }
 }
